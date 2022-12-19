3f1b3f98c244cd7e565a9aa9c3e20024dee6807b
lucasbitencourt023 <lucasbitencourt2030@gmail.com>
2022-12-19 11:09:28 -0300

    add assets

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ function creteGame(player1, hour, player2) {
     </li>
 `
 }
-let delay = -0.4
+let delay= -0.4;
 function createCard(date, day, games) {
-  deley = delay + 0.4
+  deley = delay + 0.4;
   return `
   
     <div class="card" style="animation-delay:${delay}s">
@@ -21,25 +21,23 @@ function createCard(date, day, games) {
 `
 }
 
-document.querySelector("#cards").innerHTML =
-  createCard(
-    "24/11",
-    "quinta",
-    creteGame("switzerland", "07:00", "cameroon") +
-      creteGame("portugal", "13:00", "ghana") +
-      creteGame("brazil", "16:00", "serbia")
-  ) +
-  createCard(
-    "28/11",
-    "segunda",
-    creteGame("korea", "10:00", "ghana") +
-      creteGame("brazil", "13:00", "switzerland") +
-      creteGame("portugal", "16:00", "uruguay")
-  ) +
-  createCard(
-    "02/12",
-    "sexta",
-    creteGame("ghana", "12:00", "uruguay") +
-      creteGame("serbia", "16:00", "switzerland") +
-      creteGame("brazil", "16:00", "cameroon")
-  )
+document.querySelector("#cards").innerHTML = 
+    
+      
+        createCard("24/11", "quinta",
+            creteGame("switzerland", "07:00", "cameroon") +
+            creteGame("portugal", "13:00", "ghana") +
+            creteGame("brazil", "16:00", "serbia")
+        )+
+        createCard( "28/11", "segunda",
+            creteGame("korea", "10:00", "ghana") +
+            creteGame("brazil", "13:00", "switzerland") +
+            creteGame("portugal", "16:00", "uruguay")
+        )+
+        createCard( "02/12", "sexta",
+            creteGame("ghana", "12:00", "uruguay") +
+            creteGame("serbia", "16:00", "switzerland") +
+            creteGame("brazil", "16:00", "cameroon")
+        )
+      
+
